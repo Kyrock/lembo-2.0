78c034b1d1a876b403091cf681c2c3f6ab360037
Kyrock04 <santiempresariyluna@gmail.com>
2025-03-27 15:41:09 -0500

    Creacion de server

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-// Aca va lo del script de los usuarios 
+import express from 'express';
+import mysql from 'mysql2';
+import cors from 'cors';
+const app = express();
 
+app.use(express.json());
+app.use(cors());
 
-// Aca va la parte de los cultivos 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'lembo'
+});
+db.connect((err) => {
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log('Connected to database');
+});
 
+// Handle database connection errors
+db.on('error', (err) => {
+    console.error('Database error:', err);
+});
 
-// Aca va la parte de los insumos
+app.listen(3306, () => {
+    console.log('Server is running on puerto htt://localhost:3306');
+    });
